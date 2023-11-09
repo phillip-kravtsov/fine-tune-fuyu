5e84131d97a121583d8a9539d6679b843b0205ff
Phillip Kravtsov <phillip@kravtsov.net>
2023-11-09 07:08:40 +0000

    wip

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,17 +1,17 @@
 export TOKENIZERS_PARALLELISM=false
-export CUDA_VISIBLE_DEVICES=0,1
 
-torchrun --role $(hostname -s): --tee 3 --nnodes 1 --nproc-per-node=2 --rdzv-backend=c10d --rdzv-endpoint=localhost:25500 \
+torchrun --role $(hostname -s): --tee 3 --nnodes 1 --nproc-per-node=4 --rdzv-backend=c10d --rdzv-endpoint=localhost:25500 \
 	train.py \
-  --eval_every_steps 100 \
+  --eval_every_steps 1000 \
   --save_every_steps 100 \
-  --per_device_batch_size 1 \
-  --eval_batch_size 1 \
+  --per_device_batch_size 8 \
+  --eval_batch_size 8 \
   --learning_rate 2e-5 \
   --seed 102 \
   --weight_decay 0.0 \
-  --max_eval_ids 200 \
   --model_name_or_path "fuyu-8b-slim-vocab" \
   --use_flash_attn \
+  --dataset ai2d \
 #  --profile \
 #  --use_packed_sampler \
+#  --max_eval_ids 200 \
